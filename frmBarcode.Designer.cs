f3291b1a87a30d2dd656535bf014bc84b5ef3c73
Burhan Uddin Baig <82936082+burhanuddinbaig@users.noreply.github.com>
2023-06-11 13:42:02 +0500

    uploading files

---
+++ frmBarcode.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace barCodeTest
+{
+    partial class frmBarcode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBarcode));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.printDoc = new System.Drawing.Printing.PrintDocument();
+            this.lbBarcode = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.pntPreDia = new System.Windows.Forms.PrintPreviewDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input String:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(38, 48);
+            this.txtInput.MaxLength = 16;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(138, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // printDoc
+            // 
+            this.printDoc.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDoc_PrintPage);
+            // 
+            // lbBarcode
+            // 
+            this.lbBarcode.FormattingEnabled = true;
+            this.lbBarcode.Location = new System.Drawing.Point(12, 187);
+            this.lbBarcode.Name = "lbBarcode";
+            this.lbBarcode.Size = new System.Drawing.Size(185, 199);
+            this.lbBarcode.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(38, 134);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(138, 22);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add to List";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(39, 157);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(138, 22);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.Text = "Start Printing";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // pntPreDia
+            // 
+            this.pntPreDia.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.pntPreDia.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.pntPreDia.ClientSize = new System.Drawing.Size(400, 300);
+            this.pntPreDia.Enabled = true;
+            this.pntPreDia.Icon = ((System.Drawing.Icon)(resources.GetObject("pntPreDia.Icon")));
+            this.pntPreDia.Name = "pntPreDia";
+            this.pntPreDia.Visible = false;
+            // 
+            // frmBarcode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(215, 398);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbBarcode);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "frmBarcode";
+            this.Text = "frmBarcode";
+            this.Load += new System.EventHandler(this.frmBarcode_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Drawing.Printing.PrintDocument printDoc;
+        private System.Windows.Forms.ListBox lbBarcode;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.PrintPreviewDialog pntPreDia;
+    }
+}
---
+++ frmBarcode.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OnBarcode.Barcode.WinForms;
+using OnBarcode.Barcode;
+using System.IO;
+using System.Drawing.Printing;
+
+namespace barCodeTest
+{
+    public partial class frmBarcode : Form
+    {
+        public frmBarcode()
+        {
+            InitializeComponent();
+        }
+
+        string input = "";
+        string path = "E:/barcode.bmp";
+        public const int maxrow = 8, maxcol = 4, maxpage = 10, xdif = 200, ydif = 150, xmargin = 25 , ymargin = 25;
+        public int currow = 0, curcol = 0, curpage = 0;
+        public string data = "";
+        public int tot = 0, count = 0;
+        public Image img;
+
+        private void frmBarcode_Load(object sender, EventArgs e)
+        {
+            OnBarcode.Barcode.WinForms.LinearWinForm win = new LinearWinForm();
+            win.Show();
+        }
+
+        public Image GenerateBacode(string _str)
+        {
+            Linear barcode = new Linear();
+            barcode.Type = BarcodeType.UPCA;
+            barcode.BarcodeHeight = 16;
+            barcode.UOM = UnitOfMeasure.PIXEL;
+            barcode.Data = _str;
+            barcode.Resolution = 100;
+            barcode.Rotate = Rotate.Rotate0;
+            
+            Image tmp = barcode.drawBarcode();
+            return tmp;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            input = txtInput.Text;
+            if (string.IsNullOrEmpty(input)) return;
+            lbBarcode.Items.Add(txtInput.Text);
+        }
+
+        private void printDoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.HasMorePages = true;
+            
+            int i = 0;
+            for (i = curpage * 32; i < ((curpage + 1) * 32); i++)
+            {
+                if (tot == i) { e.HasMorePages = false; break; }
+                data = lbBarcode.Items[i].ToString();
+                currow = i % maxrow;
+                curcol = (int) i/maxrow;
+                curcol -= curpage * 4;
+                img = GenerateBacode(data);
+           //     e.Graphics.DrawString("Al-Amin Stationers and Traders", new Font("Serif", 10), Brushes.Black, (xdif * curcol) + xmargin, (ydif * currow) + ymargin);
+                e.Graphics.DrawImage(img, new Point((xdif * curcol) + xmargin , (ydif * currow)+ ymargin));
+            }
+            curpage++;
+            if (e.HasMorePages) e.HasMorePages = true;
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            tot = lbBarcode.Items.Count;
+        //    pntPreDia.Document = printDoc;
+        //    pntPreDia.ShowDialog();
+            printDoc.Print();
+        }
+    }
+}
